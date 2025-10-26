9dc879b1b24701ff45e04e9bcdf508f991aef785
vsharma744 <vsharma744@gmail.com>
2025-10-26 16:47:44 +0530

    Add files via upload

---
+++ 1st assignment.py
@@ -0,0 +1,24 @@
+# Write a Python program that does the following:
+#  Takes two numbers as input from the user.
+# Performs the basic mathematical operations on these two numbers:
+#	Addition
+#	Subtraction
+#	Multiplication
+#	Division
+#  Displays the results of each operation on the screen
+
+
+
+
+
+num1 = float(input("enter 1st no: "))
+num2 = float(input("enter 2nd no: "))
+print (f"addition is{num1+num2}")
+print (f"subraction is{num1-num2}")
+print (f"multiplaction is{num1*num2}")
+print (f"division is{num1/num2}")
+
+
+name_1 = str(input("input your first name: "))
+name_2 = str(input("input your last name: "))
+print(f"Hello {name_1} "f" {name_2}! welcome to the python program.")
